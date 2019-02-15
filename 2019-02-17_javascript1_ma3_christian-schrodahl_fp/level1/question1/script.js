37017fbaa5839a547a2ad56eeea67f499cc53b2b
Christianschrodahl <christian.schrodahl@gmail.com>
2019-02-15 09:46:09 +0100

    Update script.js

--- 2019-02-17_javascript1_ma3_christian-schrodahl_fp/level1/question1/script.js
+++ 2019-02-17_javascript1_ma3_christian-schrodahl_fp/level1/question1/script.js
@@ -4,12 +4,13 @@ document.getElementById("submit").addEventListener("click", function(){
     
     for(var i = 0; i < inputFields.length; i++){
         var theInput = inputFields[i].value;
-        console.log(theInput);
         var thePattern = new RegExp(inputFields[i].pattern);
         var theTest = thePattern.test(theInput);
         
         if(theTest === false){
             console.log(inputFields[i].name + " is empty")
+        } else {
+            console.log(inputFields[i].name + ": " + theInput);
         }
     }
 })
